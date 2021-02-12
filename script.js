03955b4ef7ba1f63fb793c62a913a16f32b553b7
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-02-12 18:22:08 -0500

    Edited js

--- script.js
+++ script.js
@@ -9,63 +9,67 @@ let wantsNumeric;
 let wantsSpecialChar;
 let wantsUpperCase;
 let passwordLength;
+let magicUserPassword = ""
 
 let generateBtn = document.querySelector("#generate");
-console.log("hello!");
 
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
 //event listener for length prompt and remaining comfirmation queries follow
 generateBtn.addEventListener("click", writePassword);
 
-//restricts user from choosing a numerical value outside of the range of 8-128 and redirects them back to the initial length prompt
+
 function writePassword() {
-  function CharLength () {
-      passwordLength = prompt("Enter desired number of characters for password (8-128):");
-  if (passwordLength < 8 || passwordLength > 128 || passwordLength == "") {
-  alert("Error! Please select character length that is between 8 and 128!");
-    CharLength();
-  } 
-}
-CharLength()
-//if user doesn't select at least one of the character types it redirects them back to the beginning of the queriers
-function youMustChoose() {
-  wantsLowerCase = confirm("Do you want to include lowercase characters?");
-  wantsNumeric = confirm("Do you want to include numbers?");
-  wantsSpecialChar = confirm("Do you want to include special characters?");
-  wantsUpperCase = confirm("Do you want to include uppercase characters");
-  if (wantsLowerCase == false && wantsUpperCase == false && wantsNumeric == false && wantsSpecialChar == false){
-  alert("Error! Please select at least one character type!");
-  youMustChoose();
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  
+  function CharLength () { //restricts user from choosing a numerical value outside of the range of 8-128 and redirects them back to the initial length prompt
+    passwordLength = prompt("Enter desired number of characters for password (8-128):");
+    // console.log(passwordLength); checking if user numerical selection is correct
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength == "") {
+      alert("Error! Please select character length that is between 8 and 128!");
+      CharLength();
+    } 
+  }
+  CharLength()
+ 
+  function youMustChoose() {  //if user doesn't select at least one of the character types it redirects them back to the beginning of the queriers
+    wantsLowerCase = confirm("Do you want to include lowercase characters?");
+    wantsNumeric = confirm("Do you want to include numbers?");
+    wantsSpecialChar = confirm("Do you want to include special characters?");
+    wantsUpperCase = confirm("Do you want to include uppercase characters");
+    if (wantsLowerCase == false && wantsUpperCase == false && wantsNumeric == false && wantsSpecialChar == false){
+    alert("Error! Please select at least one character type!");
+    youMustChoose();
+    }
+  }
+  
+  youMustChoose(); //pulling character types from arrays (currently viewable in inspect console)
+  if (wantsLowerCase) {
+    AllPossibleChars += lowercase;
+    console.log(AllPossibleChars);
+  }
+  if (wantsNumeric) {
+    AllPossibleChars += numeric;
+    console.log(AllPossibleChars);
+  }
+  if (wantsSpecialChar) {
+    AllPossibleChars += specialChar;
+    console.log(AllPossibleChars);
+  }
+  if (wantsUpperCase) {
+    AllPossibleChars += uppercase;
+    console.log(AllPossibleChars);
   }
 }
-youMustChoose();
 
-//pulling character types from arrays (currently viewable in inspect console)
-if (wantsLowerCase) {
-  AllPossibleChars += lowercase;
-  console.log(AllPossibleChars);
-}
-if (wantsNumeric) {
-  AllPossibleChars += numeric;
-  console.log(AllPossibleChars) * Math.floor(Math.random);
-}
-if (wantsSpecialChar) {
-  AllPossibleChars += specialChar;
-  console.log(AllPossibleChars);
-}
-if (wantsUpperCase) {
-  AllPossibleChars += uppercase;
-  console.log(AllPossibleChars);
-}
-function generatePassword() {
-  return;
-}
-
-//what actually spites out the password
-}
+  function generatePassword() { //what actually spits out the password
+    for (let i = 0; i < passwordLength.length; i++); {
+      const element = AllPossibleChars[i];
+      console.log("There is no Dana only coooooode!");        
+      }
+    }
+    
+    magicUserPassword
+    return ;
+  
